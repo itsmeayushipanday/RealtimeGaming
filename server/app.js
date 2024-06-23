300c13a53a8f39dfcb010223de60539293e488f2
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-06-23 14:31:40 +0530

    Implemented the cors

--- server/app.js
+++ server/app.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  })
+);
+
 const httpServer = createServer(app);
-const io = new Server(httpServer, {});
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  },
+});
 
 io.on("connection", (socket) => {
-  console.log(socket);
+  console.log("New client connected:", socket.id);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
 });
 
 httpServer.listen(3000, () => {
-  console.log("Server started");
+  console.log("Server started on port 3000");
 });
