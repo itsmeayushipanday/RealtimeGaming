71764a1e5804fe31afa297bb1be8ba50199710b3
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-07-03 22:08:10 +0530

    fixes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,12 @@
-// import Board from "./components/Board";
-import Home from "./components/Home";
+import Board from "./components/Board";
+// import Home from "./components/Home";
 import "./styles.css";
 
 function App() {
   return (
     <div>
-      {/* <Board /> */}
-      <Home />
+      <Board />
+      {/* <Home /> */}
     </div>
   );
 }
